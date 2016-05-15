6122c86d63ecd489924194431158fcc993143422
darkzero <darkzero_mk2@hotmail.com>
2016-05-15 23:07:17 +0900

    create file

---
+++ DZPopupMessageView/Classes/DZPopupMessageView.swift
@@ -0,0 +1,21 @@
+//
+//  DZPopupMessageView.swift
+//  Pods
+//
+//  Created by darkzero on 16/5/15.
+//
+//
+
+import UIKit
+
+class DZPopupMessageView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
